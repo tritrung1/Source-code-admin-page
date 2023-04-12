efd32aee165a862b5c111feeddd5b4f14d5f5dc0
tritrung1 <tritrung24897@gmail.com>
2023-04-12 20:51:17 +0700

    Fisrt commit

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,89 @@
+$(document).ready(function () {
+    $("#orderList").click(function () {
+        $.get({
+            url: "orders-list-management.html",
+            success: function (response) {
+                $("#container").html(response);
+            }
+        });
+    });
+    $("#productsList").click(function () {
+        $.get({
+            url: "products.html",
+            success: function (response) {
+                $("#container").html(response);
+            }
+        });
+    })
+    $("#addProduct").click(function () {
+        $.get({
+            url: "add-product.html",
+            success: function (response) {
+                $("#container").html(response);
+            }
+        });
+    });
+    $("#notificationList").click(function () {
+        $.get({
+            url: "notification-management.html",
+            success: function (response) {
+                $("#container").html(response);
+            }
+        });
+    });
+    $("#accountList").click(function () {
+        $.get({
+            url: "accounts.html",
+            success: function (response) {
+                $("#container").html(response);
+            }
+        });
+    })
+    $("#postManagement").click(function () {
+        $.get({
+            url: "post-management.html",
+            success: function (response) {
+                $("#container").html(response);
+            }
+        });
+    })
+    $("#addProduct").click(function () {
+        $.get({
+            url: "add-product.html",
+            success: function (response) {
+                $("#container").html(response);
+            }
+        });
+    });
+
+    $(".tm-product-name").click(function () {
+        $.get({
+            url: "edit-product.html",
+            success: function (response) {
+                $("#container").html(response);
+            }
+        });
+    });
+    $(".tm-product-update-link").click(function () {
+        $.get({
+            url: "update-user-form.html",
+            success: function (response) {
+                $("#container").html(response);
+            }
+        });
+    });
+    $("#createNewAccount").click(function () {
+        $.get({
+            url: "create-user-form.html",
+            success: function (response) {
+                $("#container").html(response);
+            }
+        });
+    });
+
+    $('.nav-item').click(function(){
+        $('.nav-item').removeClass("active");
+        $(this).addClass("active");
+    });
+
+})
