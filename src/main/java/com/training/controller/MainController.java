9351f35471a696fdd187e84330efb023434267cf
tritrung1 <tritrung24897@gmail.com>
2023-04-14 20:20:21 +0700

    update 8h30

--- src/main/java/com/training/controller/MainController.java
+++ src/main/java/com/training/controller/MainController.java
@@ -15,49 +15,49 @@ public class MainController {
 
     @RequestMapping(value = {"/", "/index"})
     public String getInit(Model model) {
-        model.addAttribute("direction","components/init");
+        model.addAttribute("direction","container/init");
         return "index";
     }
 
     @RequestMapping("/products")
     public String products(Model model) {
-        model.addAttribute("direction","components/product");
+        model.addAttribute("direction","container/product");
         return "index";
     }
 
     @RequestMapping("/accounts")
     public String accounts(Model model) {
-        model.addAttribute("direction","components/accounts");
+        model.addAttribute("direction","container/accounts");
         return "index";
     }
 
     @RequestMapping("/post-manage")
     public String postManage(Model model) {
-        model.addAttribute("direction","components/post-manage");
+        model.addAttribute("direction","container/post-manage");
         return "index";
     }
 
     @RequestMapping("/order-list-manage")
     public String orderList(Model model) {
-        model.addAttribute("direction","components/order-list-manage");
+        model.addAttribute("direction","container/order-list-manage");
         return "index";
     }
 
     @RequestMapping("/notification-manage")
     public String notification(Model model) {
-        model.addAttribute("direction","components/notification-manage");
+        model.addAttribute("direction","container/notification-manage");
         return "index";
     }
 
     @RequestMapping("/create-account")
     public String createAccount(Model model) {
-        model.addAttribute("direction","components/accounts");
+        model.addAttribute("direction","container/accounts");
         return "index";
     }
 
     @RequestMapping("/add-product")
     public String addProduct(Model model) {
-        model.addAttribute("direction","components/add-product");
+        model.addAttribute("direction","container/add-product");
         return "index";
     }
 
